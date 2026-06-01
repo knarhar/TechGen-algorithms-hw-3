0648eabcf673506a0327bd5bff7f71518cd8ebe8
knarhar <harutyunyanqnarik2@gmail.com>
2026-06-01 17:14:50 +0400

    feat: add the solution for first task

--- TechGen-algorithms-hw-3/Program.cs
+++ TechGen-algorithms-hw-3/Program.cs
@@ -2,9 +2,81 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        #region TASK 1
+        private static bool Matching(char chOpen, char chClose)
         {
-            Console.WriteLine("Hello, World!");
+            switch (chOpen)
+            {
+                case '(':
+                    if (chClose == ')')
+                        return true;
+                    return false;
+                case '[':
+                    if (chClose == ']')
+                        return true;
+                    return false;
+                case '{':
+                    if (chClose == '}')
+                        return true;
+                    return false;
+                default:
+                    return false;
+            }
+        }
+
+        static bool CheckBraces(string text)
+        {
+            if (String.IsNullOrEmpty(text))
+            {
+                return false;
+            }
+
+            // my stack logic used for char elements
+            char[] stack = new char[text.Length];
+            int top = -1;
+
+            foreach (char ch in text)
+            {
+                // save the opened braces, and check on closing ones
+                if (ch == '(' || ch == '[' || ch == '{')
+                {
+                    stack[++top] = ch;
+                }
+                else
+                {
+                    if (top == -1)
+                    {
+                        return false;
+                    }
+
+                    char opened = stack[top--];
+
+                    if (!Program.Matching(opened, ch))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return top == -1;
+        }
+
+        #endregion TASK 1
+
+
+        static void Main()
+        {
+            // --------- Test Cases for Task 1 ---------
+            // ● "()" → true
+            // ● "([])" → true
+            // ● "([)]" → false
+            // ● "{[()()]}" → true
+            // ● "(((" → false
+            Console.WriteLine(CheckBraces("()"));
+            Console.WriteLine(CheckBraces("([])"));
+            Console.WriteLine(CheckBraces("([)]"));
+            Console.WriteLine(CheckBraces("{[()()]}"));
+            Console.WriteLine(CheckBraces("((("));
         }
     }
 }
