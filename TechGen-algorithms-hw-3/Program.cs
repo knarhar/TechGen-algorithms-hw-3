eae3e6ca01c81d4469681c75f174319586af7859
knarhar <harutyunyanqnarik2@gmail.com>
2026-06-01 16:48:32 +0400

    Add project files.

---
+++ TechGen-algorithms-hw-3/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TechGen_algorithms_hw_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
